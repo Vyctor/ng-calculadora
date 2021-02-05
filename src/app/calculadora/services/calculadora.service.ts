b4ce4d350907ca2fa46a6359e38b7689a42496a4
Vyctor <dev.vyctor@gmail.com>
2021-02-04 21:05:14 -0300

    feat: implementando o metodo calcular, de calculadora service

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -5,5 +5,26 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
+
+  public calcular(numeroA: number, numeroB: number, operacao: string): number {
+    switch (operacao) {
+      case CalculadoraService.SOMA:
+        return numeroA + numeroB;
+      case CalculadoraService.SUBTRACAO:
+        return numeroA - numeroB;
+      case CalculadoraService.MULTIPLICACAO:
+        return numeroA * numeroB;
+      case CalculadoraService.DIVISAO:
+        return numeroA / numeroB;
+      default:
+        return 0;
+    }
+  }
+
 }
