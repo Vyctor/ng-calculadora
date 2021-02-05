daf4eeb1f3d047f0e395251aa64ef119df741dc4
Vyctor <dev.vyctor@gmail.com>
2021-02-04 21:16:35 -0300

    feat: implementando testes

--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
 
@@ -13,4 +13,24 @@ describe('CalculadoraService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return 2 when 1 + 1', inject([CalculadoraService], () => {
+    const sum = service.calcular(1, 1, CalculadoraService.SOMA);
+    expect(sum).toEqual(2);
+  }));
+
+  it('should return 0 when 1 - 1', inject([CalculadoraService], () => {
+    const sub = service.calcular(1, 1, CalculadoraService.SUBTRACAO);
+    expect(sub).toEqual(0);
+  }));
+
+  it('should return 1 when 1 / 1', inject([CalculadoraService], () => {
+    const div = service.calcular(1, 1, CalculadoraService.DIVISAO);
+    expect(div).toEqual(1);
+  }));
+
+  it('should return 1 when 1 * 1', inject([CalculadoraService], () => {
+    const mult = service.calcular(1, 1, CalculadoraService.MULTIPLICACAO);
+    expect(mult).toEqual(1);
+  }));
 });
